11d05bf80396853a289f4ae38ec96c6e47ebed18
Vince Carpino <carpi111@mail.chapman.edu>
2018-11-11 14:26:42 -0800

    Add dummy file to hopefully get this folder pushed

---
+++ app/src/main/java/com/chapman/dev/vincecarpino/final_project/DummyActivity.java
@@ -0,0 +1,7 @@
+package com.chapman.dev.vincecarpino.final_project;
+
+import android.app.Activity;
+
+public class DummyActivity extends Activity {
+    // This is really dumb hope this works tho
+}
