df226d4ddba31c5f0cc22495aa7a524fd451692c
Goldie Malamud <malam100@mail.chapman.edu>
2018-11-12 15:29:25 -0800

    added untracked files

---
+++ app/src/main/java/com/chapman/dev/vincecarpino/final_project/LoginActivity.java
@@ -0,0 +1,17 @@
+package com.chapman.dev.vincecarpino.final_project;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.BottomNavigationView;
+
+public class LoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+
+    }
+}
