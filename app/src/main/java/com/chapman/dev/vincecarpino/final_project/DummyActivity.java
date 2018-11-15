34df3d81ec3086b5ae3b33adefb23b4ebed36ae0
Goldie Malamud <malam100@mail.chapman.edu>
2018-11-14 18:16:35 -0800

    adds login background img & incomplete font

--- app/src/main/java/com/chapman/dev/vincecarpino/final_project/DummyActivity.java
+++
@@ -1,7 +0,0 @@
-package com.chapman.dev.vincecarpino.final_project;
-
-import android.app.Activity;
-
-public class DummyActivity extends Activity {
-    // This is really dumb hope this works tho
-}
