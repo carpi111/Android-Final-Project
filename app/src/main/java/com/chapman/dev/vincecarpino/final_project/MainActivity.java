42f8ac24101559dfb9a97e47e90dad6bdbf46dd0
Vince Carpino <carpi111@mail.chapman.edu>
2018-11-19 21:29:39 -0800

    Adds Profile fragment

--- app/src/main/java/com/chapman/dev/vincecarpino/final_project/MainActivity.java
+++ app/src/main/java/com/chapman/dev/vincecarpino/final_project/MainActivity.java
@@ -1,10 +1,14 @@
 package com.chapman.dev.vincecarpino.final_project;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.util.Log;
 import android.view.MenuItem;
 
@@ -25,19 +29,27 @@ public class MainActivity extends Activity {
     private BottomNavigationView.OnNavigationItemSelectedListener listener = new BottomNavigationView.OnNavigationItemSelectedListener() {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            FragmentManager fragmentManager = getFragmentManager();
+            FragmentTransaction transaction = fragmentManager.beginTransaction();
+
             switch (menuItem.getItemId()) {
                 case (R.id.navigation_profile):
-                    Log.e("MAIN","CLICKED PROFILE");
+                    transaction.replace(R.id.container, new ProfileActivity());
+                    transaction.commit();
                     break;
                 case (R.id.navigation_home):
                     Log.e("MAIN","CLICKED HOME");
+                    transaction.replace(R.id.container, new ProfileActivity());
+                    transaction.commit();
                     break;
                 case (R.id.navigation_search):
                     Log.e("MAIN","CLICKED SEARCH");
+                    transaction.replace(R.id.container, new ProfileActivity());
+                    transaction.commit();
                     break;
             }
 
-             return true;
+            return true;
         }
     };
 }
--- app/src/main/java/com/chapman/dev/vincecarpino/final_project/ProfileActivity.java
+++ app/src/main/java/com/chapman/dev/vincecarpino/final_project/ProfileActivity.java
@@ -1,14 +1,23 @@
 package com.chapman.dev.vincecarpino.final_project;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class ProfileActivity extends Activity {
+public class ProfileActivity extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.profile, container, false);
+    }
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.profile);
     }
 }
