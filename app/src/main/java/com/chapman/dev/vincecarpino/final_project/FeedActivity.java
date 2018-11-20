f5afb4ee68b53dffd315094b10bf0eda2e4c8e93
Goldie Malamud <malam100@mail.chapman.edu>
2018-11-19 19:43:48 -0800

    activated loginBtn

---
+++ app/src/main/java/com/chapman/dev/vincecarpino/final_project/FeedActivity.java
@@ -0,0 +1,14 @@
+package com.chapman.dev.vincecarpino.final_project;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+public class FeedActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.feed);
+    }
+}
--- app/src/main/java/com/chapman/dev/vincecarpino/final_project/LoginActivity.java
+++ app/src/main/java/com/chapman/dev/vincecarpino/final_project/LoginActivity.java
@@ -2,16 +2,47 @@ package com.chapman.dev.vincecarpino.final_project;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends Activity {
+
+    private Button loginBtn;
+    private EditText usernameInput;
+    private EditText passwordInput;
+    private TextView createAccountText;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+
+        loginBtn = findViewById(R.id.loginBtn);
+        usernameInput = findViewById(R.id.usernameInput);
+        passwordInput = findViewById(R.id.passwordInput);
+        createAccountText = findViewById(R.id.createAccountText);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent goToFeed = new Intent(getApplicationContext(), FeedActivity.class);
+                startActivity(goToFeed);
+            }
+        });
+
+        createAccountText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
 
     }
 }
