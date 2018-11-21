50505d732992221118f01ada45ae29f0d9ecb69a
Goldie Malamud <malam100@mail.chapman.edu>
2018-11-20 18:15:29 -0800

    adds login dialog layout

--- app/src/main/java/com/chapman/dev/vincecarpino/final_project/LoginActivity.java
+++ app/src/main/java/com/chapman/dev/vincecarpino/final_project/LoginActivity.java
@@ -2,10 +2,13 @@ package com.chapman.dev.vincecarpino.final_project;
 
 import android.app.ActionBar;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -37,12 +40,33 @@ public class LoginActivity extends Activity {
             }
         });
 
-        createAccountText.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-            }
-        });
+//        createAccountText.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                public Dialog onCreateDialog(Bundle savedInstanceState) {
+//                    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+//                    // Get the layout inflater
+//                    LayoutInflater inflater = getActivity().getLayoutInflater();
+//
+//                    // Inflate and set the layout for the dialog
+//                    // Pass null as the parent view because its going in the dialog layout
+//                    builder.setView(inflater.inflate(R.layout.dialog_signin, null))
+//                            // Add action buttons
+//                            .setPositiveButton(R.string.signin, new DialogInterface.OnClickListener() {
+//                                @Override
+//                                public void onClick(DialogInterface dialog, int id) {
+//                                    // sign in the user ...
+//                                }
+//                            })
+//                            .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+//                                public void onClick(DialogInterface dialog, int id) {
+//                                    LoginDialogFragment.this.getDialog().cancel();
+//                                }
+//                            });
+//                    return builder.create();
+//                }
+//            }
+//        });
 
     }
 }
