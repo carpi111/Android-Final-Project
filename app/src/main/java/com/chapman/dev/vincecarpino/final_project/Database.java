53c69aa69c50dedb49047f69663856c0617cf261
Vince Carpino <carpi111@mail.chapman.edu>
2018-12-02 14:37:12 -0800

    Adds basic code cleanup to Database class

--- app/src/main/java/com/chapman/dev/vincecarpino/final_project/Database.java
+++ app/src/main/java/com/chapman/dev/vincecarpino/final_project/Database.java
@@ -3,10 +3,23 @@ package com.chapman.dev.vincecarpino.final_project;
 import android.content.Context;
 import android.database.sqlite.*;
 
-import java.util.ArrayList;
-
 public class Database extends SQLiteOpenHelper {
+    private SQLiteStatement stmt;
     private static final String DATABASE_NAME = "ChappyFAFS";
+    private static final String[] categories = {
+            "Art",
+            "Books",
+            "Clothing",
+            "Crafts",
+            "Electronics",
+            "Everything else",
+            "Furniture",
+            "Health & Beauty",
+            "Jewelry",
+            "Musical Instruments",
+            "Real Estate",
+            "Sporting Goods"
+    };
 
     // deleteFromTable
 
@@ -18,15 +31,35 @@ public class Database extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+        createUserTable();
+
+        createProductTable();
+
+        createCategoryTable();
+
+        if (getCountOfCategoryTable() != categories.length) {
+            String sql = "DROP TABLE IF EXISTS Category;";
+            stmt = this.getWritableDatabase().compileStatement(sql);
+            stmt.executeUpdateDelete();
+
+            createCategoryTable();
+
+            populateCategoryTable();
+        }
+    }
+
+    private void createUserTable() {
         String sql = "CREATE TABLE IF NOT EXISTS User("
                 + "ID INTEGER PRIMARY KEY AUTOINCREMENT,"
                 + " Username VARCHAR,"
                 + " Password VARCHAR,"
                 + " Rating DECIMAL(1,1));";
-        SQLiteStatement stmt = this.getWritableDatabase().compileStatement(sql);
+        stmt = this.getWritableDatabase().compileStatement(sql);
         stmt.execute();
+    }
 
-        sql = "CREATE TABLE IF NOT EXISTS Product("
+    private void createProductTable() {
+        String sql = "CREATE TABLE IF NOT EXISTS Product("
                 + "ID INTEGER PRIMARY KEY AUTOINCREMENT,"
                 + " Name VARCHAR,"
                 + " Description VARCHAR,"
@@ -35,8 +68,10 @@ public class Database extends SQLiteOpenHelper {
                 + " Price DECIMAL(4,2));";
         stmt = this.getWritableDatabase().compileStatement(sql);
         stmt.execute();
+    }
 
-        sql = "CREATE TABLE IF NOT EXISTS Category("
+    private void createCategoryTable() {
+        String sql = "CREATE TABLE IF NOT EXISTS Category("
                 + "ID INTEGER PRIMARY KEY AUTOINCREMENT,"
                 + "Name VARCHAR);";
         stmt = this.getWritableDatabase().compileStatement(sql);
@@ -44,12 +79,9 @@ public class Database extends SQLiteOpenHelper {
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) { }
 
-    }
-
-    public void insertIntoUser(String username, String password, float rating)
-    {
+    public void insertIntoUser(String username, String password, float rating) {
         String sql = "INSERT INTO User(Username, Password, Rating) VALUES(?,?,?);";
         SQLiteStatement stmt = this.getReadableDatabase().compileStatement(sql);
         stmt.bindString(1, username);
@@ -59,8 +91,7 @@ public class Database extends SQLiteOpenHelper {
         stmt.executeInsert();
     }
 
-    public void insertIntoProduct(String name, String description, int categoryID, int sellerID, float price)
-    {
+    public void insertIntoProduct(String name, String description, int categoryID, int sellerID, float price) {
         String sql = "INSERT INTO Product(Name, Desription, CategoryID, SellerID, Price) VALUES(?,?,?,?,?);";
         SQLiteStatement stmt = this.getReadableDatabase().compileStatement(sql);
         stmt.bindString(1, name);
@@ -72,30 +103,30 @@ public class Database extends SQLiteOpenHelper {
         stmt.executeInsert();
     }
 
-    public void makeCategoryTable()
-    {
-        String[] categories = {"Art", "Books", "Clothing", "Crafts", "Electronics", "Everything else",
-        "Furniture", "Health & Beauty", "Jewelry", "Musical Instruments", "Real Estate", "Sporting Goods"};
-
-        for (String c : categories)
-        {
+    public void populateCategoryTable() {
+        for (String c : categories) {
             String sql = "INSERT INTO Category(Name) VALUES(?);";
             SQLiteStatement stmt = this.getReadableDatabase().compileStatement(sql);
-            stmt.bindString(1,c);
+            stmt.bindString(1, c);
 
             stmt.executeInsert();
         }
     }
 
-    public void deleteProduct(int id)
-    {
+    private long getCountOfCategoryTable() {
+        String sql = "SELECT COUNT(*) FROM Category;";
+        SQLiteStatement stmt = this.getReadableDatabase().compileStatement(sql);
+
+        return stmt.simpleQueryForLong();
+    }
+
+    public void deleteProduct(int id) {
         String sql = "DELETE FROM Product WHERE ID=" + String.valueOf(id);
         SQLiteStatement stmt = this.getReadableDatabase().compileStatement(sql);
         stmt.executeInsert();
     }
 
-    public String getUsername(int id)
-    {
+    public String getUsername(int id) {
         String sql = "SELECT Username FROM User WHERE ID=" + String.valueOf(id);
         SQLiteStatement stmt = this.getReadableDatabase().compileStatement(sql);
         String username = stmt.simpleQueryForString();
@@ -111,6 +142,4 @@ public class Database extends SQLiteOpenHelper {
 //        SQLiteStatement stmt = this.getReadableDatabase().compileStatement(sql);
 //        //not finished
 //    }
-
-
 }
