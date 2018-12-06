12260e2e06d18ab5bea9e5088b297f42c1c5cc16
Vince Carpino <carpi111@mail.chapman.edu>
2018-12-06 14:53:25 -0800

    Adds options menu to profile page

--- app/src/main/java/com/chapman/dev/vincecarpino/final_project/ProfileFragment.java
+++ app/src/main/java/com/chapman/dev/vincecarpino/final_project/ProfileFragment.java
@@ -1,24 +1,19 @@
 package com.chapman.dev.vincecarpino.final_project;
 
-import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.view.ContextMenu;
 import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.ImageButton;
 
 public class ProfileFragment extends Fragment {
-
-    private ImageButton profileNewItemBtn;
 
     private static final int ADD_ITEM_ID  = Menu.FIRST;
     private static final int EDIT_PROF_ID = Menu.FIRST + 1;
@@ -26,40 +21,39 @@ public class ProfileFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-
-        View rootView = inflater.inflate(R.layout.profile, container, false);
-        profileNewItemBtn = rootView.findViewById(R.id.profileNewItemBtn);
-
-        profileNewItemBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FragmentManager fragmentManager = getFragmentManager();
-                FragmentTransaction transaction = fragmentManager.beginTransaction();
-
-                transaction.replace(R.id.container, new AddItemFragment());
-                transaction.commit();
-            }
-        });
-
-        return rootView;
+        return inflater.inflate(R.layout.profile, container, false);
     }
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-
+        setHasOptionsMenu(true);
     }
 
     @Override
-    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         menu.add(Menu.NONE, ADD_ITEM_ID, Menu.NONE, "ADD ITEM");
         menu.add(Menu.NONE, EDIT_PROF_ID, Menu.NONE, "EDIT PROFILE");
-        super.onCreateContextMenu(menu, v, menuInfo);
+        super.onCreateOptionsMenu(menu, inflater);
     }
 
     @Override
-    public boolean onContextItemSelected(MenuItem item) {
-        return super.onContextItemSelected(item);
+    public boolean onOptionsItemSelected(MenuItem item) {
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+
+        switch (item.getItemId()) {
+            case ADD_ITEM_ID:
+                transaction.replace(R.id.container, new AddItemFragment());
+                break;
+            case EDIT_PROF_ID:
+                // TODO: Edit profile page
+                break;
+        }
+
+        transaction.commit();
+
+        return true;
     }
 }
