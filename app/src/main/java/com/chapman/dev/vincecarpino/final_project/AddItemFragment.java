4781f4eefa33ca804324db49ade9fb726f5798f6
Vince Carpino <carpi111@mail.chapman.edu>
2018-12-09 13:44:41 -0800

    Adds functionality to Save New Item button

--- app/src/main/java/com/chapman/dev/vincecarpino/final_project/AddItemFragment.java
+++ app/src/main/java/com/chapman/dev/vincecarpino/final_project/AddItemFragment.java
@@ -4,36 +4,47 @@ import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.text.InputFilter;
-import android.text.Spanned;
-import android.text.style.TtsSpan;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Spinner;
 
-import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class AddItemFragment extends Fragment {
-    private Spinner categorySpinner;
-    private EditText dollarInput;
-
+    private EditText    itemNameInput;
+    private EditText    itemPriceInput;
+    private Spinner     categorySpinner;
+    private EditText    itemDescInput;
+    private ImageButton itemSaveButton;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.new_item, container, false);
+        View rootView   = inflater.inflate(R.layout.new_item, container, false);
+
+        itemNameInput   = rootView.findViewById(R.id.newItemNameInput);
+        itemPriceInput  = rootView.findViewById(R.id.newItemPriceInput);
         categorySpinner = rootView.findViewById(R.id.newItemCategorySpinner);
-        dollarInput = rootView.findViewById(R.id.dollarInput);
-        //dollarInput.setFilters(new InputFilter[] {new DecimalDigitsInputFilter(3,2)});
+        itemDescInput   = rootView.findViewById(R.id.newItemDescriptionInput);
+        itemSaveButton  = rootView.findViewById(R.id.newItemSaveButton);
+
         setUpCategorySpinner();
+        setUpSaveButton();
+
+        itemSaveButton.setEnabled(false);
+
+        itemNameInput.addTextChangedListener(watcher);
+        itemPriceInput.addTextChangedListener(watcher);
+        itemDescInput.addTextChangedListener(watcher);
 
         return rootView;
     }
@@ -41,16 +52,12 @@ public class AddItemFragment extends Fragment {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
     }
 
-    public void setUpCategorySpinner()
-    {
-        List<String> list = new ArrayList<>();
-        ArrayList<String> categories = new ArrayList<>();
+    public void setUpCategorySpinner() {
         ArrayAdapter<String> adapter;
 
-        String[] allCategories = new String[]{
+        String[] allCategories = new String[] {
                 "All Categories",
                 "Art",
                 "Books",
@@ -66,9 +73,8 @@ public class AddItemFragment extends Fragment {
                 "Sporting Goods"
         };
 
-
-        categories.addAll(Arrays.asList(allCategories));
-        list.addAll(categories);
+        ArrayList<String> categories = new ArrayList<>(Arrays.asList(allCategories));
+        List<String> list = new ArrayList<>(categories);
 
         adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_spinner_item, list);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -76,23 +82,49 @@ public class AddItemFragment extends Fragment {
         categorySpinner.setAdapter(adapter);
     }
 
-//    public class DecimalDigitsInputFilter implements InputFilter {
-//
-//        Pattern mPattern;
-//
-//        public DecimalDigitsInputFilter(int digitsBeforeZero,int digitsAfterZero) {
-//            mPattern=Pattern.compile("[0-9]{0," + (digitsBeforeZero-1) + "}+((\\.[0-9]{0," + (digitsAfterZero-1) + "})?)||(\\.)?");
-//        }
-//
-//        @Override
-//        public CharSequence filter(CharSequence source, int start, int end, Spanned dest, int dstart, int dend) {
-//
-//            Matcher matcher=mPattern.matcher(dest);
-//            if(!matcher.matches())
-//                return "";
-//            return null;
-//        }
-//
-//    }
+    private void setUpSaveButton() {
+        itemSaveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String newItemName = getEditTextString(itemNameInput);
+                String newItemPrice = getEditTextString(itemPriceInput);
+                String newItemCat = categorySpinner.getSelectedItem().toString();
+                String newItemDesc = getEditTextString(itemDescInput);
 
+
+
+                Log.e("NEW ITEM", getEditTextString(itemNameInput));
+                Log.e("NEW ITEM", getEditTextString(itemPriceInput));
+                Log.e("NEW ITEM", categorySpinner.getSelectedItem().toString());
+                Log.e("NEW ITEM", getEditTextString(itemDescInput));
+            }
+        });
+    }
+
+    private boolean anyInputsAreEmpty() {
+        return getEditTextString(itemNameInput).equals("")
+                || getEditTextString(itemPriceInput).equals("")
+                || getEditTextString(itemDescInput).equals("");
+    }
+
+    private String getEditTextString(EditText e) {
+        return e.getText().toString();
+    }
+
+    TextWatcher watcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) { }
+
+        @Override
+        public void afterTextChanged(Editable s) {
+            if (anyInputsAreEmpty()) {
+                itemSaveButton.setEnabled(false);
+            } else {
+                itemSaveButton.setEnabled(true);
+            }
+        }
+    };
 }
